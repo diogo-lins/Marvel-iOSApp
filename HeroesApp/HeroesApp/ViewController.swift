037e7ffc63a8ebe4567b9d46ae32658fdba6f9d5
Diogo Moreira <dmoreira@thoughtworks.com>
2020-12-07 14:00:38 -0300

    chore: Add initial project files

---
+++ HeroesApp/HeroesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroesApp
+//
+//  Created by Diogo Moreira on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

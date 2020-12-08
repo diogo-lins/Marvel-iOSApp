51f3bf5283a7bef0e89432698edb4ced376c8641
Diogo Moreira <dmoreira@thoughtworks.com>
2020-12-08 08:45:37 -0300

    refactor: to change arch domains and create firsts folders modules in code project

---
+++ HeroesApp/HeroesApp/App/HeroNetworking/HeroDataAdapter.swift
@@ -0,0 +1,11 @@
+//
+//  HeroDataAdapter.swift
+//  HeroesApp
+//
+//  Created by Diogo Moreira on 07/12/20.
+//
+
+import Foundation
+
+struct HeroDataAdapter: RemoteService {
+}
---
+++ HeroesApp/HeroesApp/App/Service/RemoteService.swift
@@ -0,0 +1,11 @@
+//
+//  RemoteService.swift
+//  HeroesApp
+//
+//  Created by Diogo Moreira on 07/12/20.
+//
+
+import Foundation
+
+protocol RemoteService {
+}
